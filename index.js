8c3597c7b45ae153d8222f4da5c17eebc30d3a55
queti1301 <158857269+queti1301@users.noreply.github.com>
2024-02-04 16:35:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const helloBtn = document.querySelector(".helloBtn");
+
+
+helloBtn.addEventListener("click", () => {
+    question.innerHTML = "Esto se ve increiblemente tonto pero creeme q si me costo esto, nunca me arrepenti tanto de no prestar atención a las clases del profesor Gerald aunq no enseño nada pero bueno, lo di todo jsfhjas los botones me salen chuecos lloro";
+    
+
+    helloBtn.addEventListener("click", () => {
+        question.innerHTML = "Sinceramente no sabia muy bien que regalarte pero tampoco es que no quisiera darte nada porque despues de todo ya estamos cumpliendo más de 1 mes sigo sin creerlo a ser sincera pero realmente estoy demasiado feliz";
+    
+
+        helloBtn.addEventListener("click", () => {
+            question.innerHTML = "Bueno, no es el regalo más elaborado del mundo aunq lo más probable esque te de algo más a parte de esto, nunca pense q fuera tan facil y complicado esta cosa lo peor esq quedo para el poto pero bueno ya se porq nunca sere ingeniera ni nd de eso equisde";
+
+            helloBtn.addEventListener("click", () => {
+                question.innerHTML = "Se podria decir que ¡Feliz más de un mes! jfhfhgaj  te quiero mucho y aunq paresca muy exagerada y pesada realmente estoy muy feliz de estar contigo :)";
+
+                helloBtn.addEventListener("click", () => {
+                    question.innerHTML = "Sigo sin saber muy bien q me viste pero realmente cuentas conmigo para cualquier cosa q necesites hacer o decir siempre pero siempre voy a estar a tu lado mientras tu me lo permitas";
+
+                    helloBtn.addEventListener("click", () => {
+                        question.innerHTML = "Bueno, eso fue todo perdón si no es la gran cosa pero lo hice con cariño chauuu te quiero mucho                                               M.Ce.ΔT"
+                    });
+                });
+            });
+        });
+        
+
+    });
+});
